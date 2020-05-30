6e0e6ac39809ec72bc81ef0cbee08a2600981803
George Makoveckij <Itemfouund@gmail.com>
2020-05-30 22:09:29 +0300

    Updates

---
+++ precache-manifest.169d6cac7e71113181cbea4e98bc010a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4783e30435c35f2cc0cae1f0828ca68c",
+    "url": "/shop-example/index.html"
+  },
+  {
+    "revision": "87b198cfbee7c1aac240",
+    "url": "/shop-example/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2e88a084fc1d3cdc4644",
+    "url": "/shop-example/static/js/2.d0385e31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/shop-example/static/js/2.d0385e31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87b198cfbee7c1aac240",
+    "url": "/shop-example/static/js/main.7dd1571d.chunk.js"
+  },
+  {
+    "revision": "a228910dc8ef8f88167b",
+    "url": "/shop-example/static/js/runtime-main.418aef6e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shop-example/static/media/logo.5d5d9eef.svg"
+  }
+]);
